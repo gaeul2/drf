472a76fdd06e9f4121b0cb1295144beb58f67d19
gaeul <e0540@hanmail.net>
2022-06-15 16:43:42 +0900

    로그인 안되는 이유찾아 수정.
    signup함수를 만들지 않고 유저생성을 admin에서 했음.
    사용자의 비밀번호가 create_user나 make_password로 해쉬화 되어있지 않기때문에
    장고가 비밀번호로 인식을 못함.
    사용자 아이디와 비밀번호를 입력받고 해당사용자의 비밀번호를 해쉬화하여 저장, 그리고 인증,로그인 방식으로 바꿈.

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from blog.models import Article, Category
 from .models import User
-
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 
@@ -12,14 +12,20 @@ class UserLoginView(APIView):
     def post(self, request):  # 로그인
         username = request.data.get('username', '')
         password = request.data.get('password', '')
-        print('아이디:', username, '패스워드:', password)
+        hashed_pass = make_password(password)
+        edit_user = User.objects.get(username=username)
+        edit_user.password = hashed_pass
+        edit_user.save()
         user = authenticate(request, username=username, password=password)
-        if not user:
+        # signup을 만들지 않고 admin에서 유저생성을 했기때문에 암호화된 비밀번호가 아니여서 장고가 인증을 못함.
+        # make_password() 함수를 통해 비밀번호를 해쉬화해서 해당유저 비밀번호를 변경하고, 인증을 시도하면 로그인 가능.
+
+        if user is None:
             return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
 
         login(request, user)
         return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
-        #authenticate 통과하지 못하는 에러아직 해결못함.
+
 
 class UserInfoView(APIView):
     def get(self, request):
